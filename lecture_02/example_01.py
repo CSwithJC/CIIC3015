6760bb7679a5824e73632a5de583c3103e36762f
csforjc <jean.mendez28@gmail.com>
2024-08-24 00:28:29 -0400

    Add initial examples for lecture 2.

---
+++ lecture_02/example_01.py
@@ -0,0 +1,24 @@
+# Comparing numbers
+print("Compare numbers:")
+x = 10
+y = 5
+
+print(x > y)  # True
+print(x < y)  # False
+print(x == y)  # False
+print(x != y)  # True
+
+# Comparing strings
+print("\nCompare Strings:")
+fruit1 = "apple"
+fruit2 = "banana"
+
+print(fruit1 == fruit2)  # False
+print(fruit1 != fruit2)  # True
+print(fruit1 < fruit2)   # True (alphabetical order)
+
+# Saving results as variables
+print("\nSave results as a variable:")
+is_apple_the_same_as_banana = fruit1 == fruit2
+print(is_apple_the_same_as_banana)
+
