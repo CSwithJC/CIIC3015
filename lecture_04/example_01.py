11c76be82b86c6155d39ee8b4fc9d9fd0b7e5f9e
csforjc <jean.mendez28@gmail.com>
2024-09-10 17:17:04 -0400

    Add example 1 for lecture 4.

---
+++ lecture_04/example_01.py
@@ -0,0 +1,18 @@
+# Print numbers 0 through 99 using a loop.
+number_of_times = 100
+index = 0
+while index < number_of_times:
+    print(index)
+    index += 1
+
+# Prompt user to enter password and keep retrying until
+# the password matches.
+
+password = ''
+correct_password = 'helloworld'
+while password != correct_password:
+    password = input('Please input password. ')
+    if password == correct_password:
+        print("Great Job!")
+    else:
+        print("Password is incorrect. Please try again.")
