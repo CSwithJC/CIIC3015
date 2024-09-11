6ff17cae3e4e372e34533264baee726575d50609
csforjc <jean.mendez28@gmail.com>
2024-09-11 11:15:48 -0400

    Add example 5 for lecture 4.

---
+++ lecture_04/example_05.py
@@ -0,0 +1,13 @@
+def print_all_characters_in_a_word(word):
+    print(f"\nPrinting all characters of {word}:")
+    for char in word:
+        print(char)
+
+# Use a for loop to iterate over a string and print each character.
+string_1 = "Colegio"
+string_2 = "Puerto Rico"
+string_3 = "Programming is awesome!"
+
+print_all_characters_in_a_word(string_1)
+print_all_characters_in_a_word(string_2)
+print_all_characters_in_a_word(string_3)
