7f5970c6bd260a38c70916bb29d986c1228ac2d4
csforjc <jean.mendez28@gmail.com>
2024-09-11 11:05:10 -0400

    Add example 4 for lecture 4.

---
+++ lecture_04/example_04.py
@@ -0,0 +1,24 @@
+# Print the length of strings.
+print(len("UPRM"))  # 4
+print(len("good morning!"))  # 13
+print(len("$10.99"))  # 6
+
+# Use the indexes of the string to print each individual character.
+print("\nIndex practice!")
+our_school = "Colegio"
+print(our_school[0])  # C
+print(our_school[1])  # o
+print(our_school[2])  # l
+print(our_school[3])  # e
+print(our_school[4])  # g
+print(our_school[5])  # i
+print(our_school[6])  # o
+
+# Use the in operator check if a character is in a string.
+print("\nin operator practice!")
+word = 'banana'
+print('a' in word)  # True
+print('b' in word)  # True
+print('z' in word)  # False
+print('y' in word)  # False
+
