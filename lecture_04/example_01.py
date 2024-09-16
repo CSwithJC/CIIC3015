e19559a6688be3e90a653c19de462806d2b7c39c
csforjc <jean.mendez28@gmail.com>
2024-09-16 15:39:42 -0400

    Added missing code from example 1 of lecture 4.

--- lecture_04/example_01.py
+++ lecture_04/example_01.py
@@ -1,3 +1,5 @@
+import time
+
 # Print numbers 0 through 99 using a loop.
 number_of_times = 100
 index = 0
@@ -17,3 +19,28 @@ while password != correct_password:
         print("Password is incorrect. Please try again.")
 
 
+# Implement a timer that prints the time remaining in a similar way to our smartphones.
+def format_remaining_time(number):
+    result = ''
+    if number < 10:
+        result += "0"
+    result += str(number)
+    return result
+
+def get_time_left(seconds):
+    minutes = seconds // 60
+    seconds %= 60
+
+    time_left = ''
+    if minutes > 0:
+        time_left += f"{format_remaining_time(minutes)}:"
+    if seconds >= 0:
+        time_left += f"{format_remaining_time(seconds)}"
+
+    print(time_left)
+
+seconds = 65
+while seconds > 0:
+    get_time_left(seconds)
+    time.sleep(1)
+    seconds -= 1
