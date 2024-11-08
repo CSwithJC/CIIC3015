a825d388c6bfdd5a1d688eb1c7e2b7a20a7de13a
csforjc <jean.mendez28@gmail.com>
2024-11-07 22:38:30 -0400

    Add example 01 of classes and temporarily move the previous example 01 code to example 02.

--- lecture_08/example_01.py
+++ lecture_08/example_01.py
@@ -1,62 +1,32 @@
-class Student:
+class Character:
     """This class models a simple student.
 
     Attributes:
-        first_name: A string of the student's first name.
-        last_name: A string of the student's last name.
-        grades: A list of integers, each of which represent a grade.
-    """
-    def __init__(self, first_name, last_name, grades):
+       name: A string of the character's name.
+       strength: A int representing the strength stat of a character.
+       defense: A int representing the defense stat of a character.
+   """
+    def __init__(self, name, strength, defense):
         """Initializes the instance based on the student's attributes.
 
         Args:
-            first_name: A string of the student's first name.
-            last_name: A string of the student's last name.
-            grades: A list of integers, each of which represent a grade.
+            name: A string of the character's name.
+            strength: A int representing the strength stat of a character.
+            defense: A int representing the defense stat of a character.
         """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.grades = grades
+        self.health = 100
+        self.name = name
+        self.strength = strength
+        self.defense = defense
 
-    def full_name(self):
-        """Returns the student's full name."""
-        return f"{self.first_name} {self.last_name}"
+    def introduce(self):
+        """Prints the character's name and stats."""
+        print(f"Hi, my name is {self.name}, my stats are: Strength: {self.strength}, Defense: {self.defense}")
 
-    def grade_point_average(self):
-        """Returns the student's grade point average based on the grades thus far."""
-        return sum(self.grades) / len(self.grades)
+# Create two new characters.
+woodie = Character(name='Woodie', strength=15, defense=7)
+pickie = Character(name='Pickie', strength=12, defense=8)
 
-    def add_grade(self, grade):
-        """Adds a new grade to the student's grades list.
-
-        Args:
-            grade: An int representing the student's new grade.
-        """
-        self.grades.append(grade)
-
-# Create a few students with the grades of their first three exams.
-michael = Student(
-    first_name='Michael',
-    last_name='Fonseca',
-    grades=[90, 80, 95]
-)
-olivia = Student(
-    first_name='Olivia',
-    last_name='Popeye',
-    grades=[95, 80, 90]
-)
-
-# Get Michael's full name and grade point average.
-print(michael.full_name())
-print(michael.grade_point_average())
-# Add a new grade to Michael's grades list and recalculate the grade average.
-michael.add_grade(75)
-print(michael.grade_point_average())
-
-# Get Olivia's full name and grade point average.
-print('\n')
-print(olivia.full_name())
-print(olivia.grade_point_average())
-# Add a new grade to Olivia's grades list and recalculate the grade average.
-olivia.add_grade(115)
-print(olivia.grade_point_average())
+# Let the characters introduce themselves.
+woodie.introduce()
+pickie.introduce()
