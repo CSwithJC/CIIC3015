7d2062017acb97e1b0b9d2f15418ba4642f793ea
csforjc <jean.mendez28@gmail.com>
2024-11-08 00:28:50 -0400

    Implement some of the code of the inheritance of game characters and add some TODOs to other files.

--- lecture_08/example_05.py
+++ lecture_08/example_05.py
@@ -1,1 +1,31 @@
-# Implement Inheritance to characters.
+# TODO: Implement Inheritance to characters.
+
+class Character:
+    def __init__(self, name):
+        self.name = name
+
+    def perform_action(self):
+        print('i have no action to perform')
+
+class SkyCharacter(Character):
+    def perform_action(self):
+        print("I can fly!")
+
+class OceanCharacter(Character):
+    def perform_action(self):
+        print("I can swim!")
+
+class GroundCharacter(Character):
+    def perform_action(self):
+        print("I can run!")
+
+sky = SkyCharacter('Sky')
+ocean = OceanCharacter('Ocean')
+ground = GroundCharacter('Ground')
+
+print(sky.name)
+print(ocean.name)
+print(ground.name)
+sky.perform_action()
+ocean.perform_action()
+ground.perform_action()
