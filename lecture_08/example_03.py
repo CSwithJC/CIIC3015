3d96e8a02260f692550c6fa4c221fe551649ecff
csforjc <jean.mendez28@gmail.com>
2024-11-10 10:43:39 -0400

    Implemented example 03 to show how the __eq__ and __str__ methods work in a class.

--- lecture_08/example_03.py
+++ lecture_08/example_03.py
@@ -1,1 +1,33 @@
-# TODO: Implement the __eq__ method and test for equality.
+class Character:
+    def __init__(self, char_name, str_score, def_score):
+        self.health = 100
+        self.name = char_name
+        self.strength = str_score
+        self.defense = def_score
+
+    """Lets us define when using the == operator on 
+    two separate Character objects returns True."""
+    def __eq__(self, other):
+        return self.name == other.name
+
+    """Lets us define the string version of this object, which includes what shows up
+    when you print it."""
+    def __str__(self):
+        return (f"\nName: {self.name}\n"
+                f"Health: {self.health}\n"
+                f"Strength: {self.strength}\n"
+                f"Defense: {self.defense}")
+
+player_1 = Character("Woodie", 15, 7)
+player_2 = Character("Woodie", 15, 7)
+player_3 = Character("Pickie", 15, 7)
+
+# The implementation of the __eq__ method determines when two separate
+# objects of Character return True when comparing them using "=="
+print(player_1 == player_2)  # Returns True because both players are called "Woodie"
+print(player_1 == player_3)  # Returns False because player 3 is called "Pickie"
+
+# The implementation of the __eq__ method determines the string value of that class,
+# which also means that it is what gets printed when you print the object.
+print(player_1)
+print(player_3)
